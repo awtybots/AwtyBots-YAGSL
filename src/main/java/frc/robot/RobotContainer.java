5c0ecd926f0ae4c703c254ae9eba5fe3fc7d114a
xeldridge18 <xeldridge13@gmail.com>
2025-01-23 18:32:11 -0600

    updated motors

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,7 +5,6 @@
 package frc.robot;
 
 import frc.robot.Constants.OIConstants;
-import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
