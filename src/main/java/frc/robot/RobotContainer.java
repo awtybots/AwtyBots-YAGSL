46ca2651bfb8f51e57bf50f8b31560e525b4c81a
DavidCui5829 <27cuiz@awty.org>
2025-03-10 17:04:43 -0500

    dribve left numper shoot

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -180,6 +180,22 @@ public class RobotContainer {
             ),
             m_coralSubsystem.runIntakeCommand(), // Do nothing
             () -> CoralSubsystem.runFunnelIntake || CoralSubsystem.ElevatorAtL4));
+    m_driverController.leftBumper().whileTrue(
+        Commands.either(
+            Commands.either(
+                Commands.parallel(
+                    m_funnelIntakeSubsystem.runIntakeCommand(), // Run Funnel Intake
+                    m_coralSubsystem.runIntakeCommand() // Run Coral Intake at the same time
+                ),
+                Commands.either(
+                    m_coralSubsystem.reverseIntakeCommand(), // If ElevatorAtL4 is true, run Reverse Intake
+                    m_coralSubsystem.runIntakeCommand(), // Otherwise, run normal intake
+                    () -> CoralSubsystem.ElevatorAtL4 // Condition for reverse intake
+                ),
+                () -> CoralSubsystem.runFunnelIntake // Condition for Funnel Intake
+            ),
+            m_coralSubsystem.runIntakeCommand(), // Do nothing
+            () -> CoralSubsystem.runFunnelIntake || CoralSubsystem.ElevatorAtL4));
 
     m_operatorController.rightStick().onTrue(m_coralSubsystem.resetElevatorEncoder());
 
@@ -204,7 +220,6 @@ public class RobotContainer {
 
     // D-Pad Down -> Elevator to 1st Algae pickup position
     m_operatorController.povDown().onTrue(m_coralSubsystem.setSetpointCommand(Setpoint.AlgaeLow));
-    
 
     m_driverController.start().onTrue(new InstantCommand(() -> drivebase.setInitialHeading(180), drivebase));
     // A Button -> Climber Goes In
