9a9beeb92a1e80c40da787337a8b9fc825b32c88
DavidCui5829 <27cuiz@awty.org>
2025-08-04 16:01:22 -0500

    changes

--- src/main/java/frc/robot/subsystems/CoralSubsystem.java
+++ src/main/java/frc/robot/subsystems/CoralSubsystem.java
@@ -49,12 +49,12 @@ public class CoralSubsystem extends SubsystemBase {
 
 
     // arm setup
-    private SparkFlex r_armMotor = new SparkFlex(ArmConstants.ArmRightCanID, MotorType.kBrushless);
-    private SparkFlex l_armMotor = new SparkFlex(ArmConstants.ArmLeftCanID, MotorType.kBrushless);
-    //private SparkClosedLoopController r_armController = r_armMotor.getClosedLoopController();
-    private SparkClosedLoopController l_armController = l_armMotor.getClosedLoopController();
+    // private SparkFlex r_armMotor = new SparkFlex(ArmConstants.ArmRightCanID, MotorType.kBrushless);
+    // private SparkFlex l_armMotor = new SparkFlex(ArmConstants.ArmLeftCanID, MotorType.kBrushless);
+    // //private SparkClosedLoopController r_armController = r_armMotor.getClosedLoopController();
+    // private SparkClosedLoopController l_armController = l_armMotor.getClosedLoopController();
     
-    private RelativeEncoder armEncoder = l_armMotor.getEncoder();
+    // private RelativeEncoder armEncoder = l_armMotor.getEncoder();
     //private AbsoluteEncoder armEncoder = l_armMotor.getAbsoluteEncoder();
     
 
@@ -75,7 +75,7 @@ public class CoralSubsystem extends SubsystemBase {
     private SparkFlex intakeMotor = new SparkFlex(ArmConstants.IntakeCanID, MotorType.kBrushless);
 
     private boolean wasReset = false;
-    private double armCurrentTarget = ArmSetpoints.FeederStation;
+    //private double armCurrentTarget = ArmSetpoints.FeederStation;
     private double wristCurrentTarget = WristSetpoints.FeederStation;
     private double elevatorCurrentTarget = ElevatorSetpoints.FeederStation;
 
@@ -93,15 +93,15 @@ public class CoralSubsystem extends SubsystemBase {
                 ResetMode.kResetSafeParameters,
                 PersistMode.kPersistParameters);
 
-        r_armMotor.configure(
-                Configs.CoralSubsystem.r_armMotorConfig,
-                ResetMode.kResetSafeParameters,
-                PersistMode.kPersistParameters);
+        // r_armMotor.configure(
+        //         Configs.CoralSubsystem.r_armMotorConfig,
+        //         ResetMode.kResetSafeParameters,
+        //         PersistMode.kPersistParameters);
 
-        l_armMotor.configure(
-                Configs.CoralSubsystem.l_armMotorConfig,
-                ResetMode.kResetSafeParameters,
-                PersistMode.kPersistParameters);
+        // l_armMotor.configure(
+        //         Configs.CoralSubsystem.l_armMotorConfig,
+        //         ResetMode.kResetSafeParameters,
+        //         PersistMode.kPersistParameters);
 
         wristMotor.configure(
                 Configs.CoralSubsystem.wristMotorConfig,
@@ -115,7 +115,7 @@ public class CoralSubsystem extends SubsystemBase {
 
         
         elevatorEncoder.setPosition(0);
-        armEncoder.setPosition(0);
+        //armEncoder.setPosition(0);
        
         
     }
@@ -139,7 +139,7 @@ public class CoralSubsystem extends SubsystemBase {
         }
         
     
-        l_armController.setReference(armCurrentTarget, ControlType.kMAXMotionPositionControl);
+        //l_armController.setReference(armCurrentTarget, ControlType.kMAXMotionPositionControl);
         wristController.setReference(wristCurrentTarget, ControlType.kMAXMotionPositionControl);
         
     }
@@ -183,47 +183,47 @@ public class CoralSubsystem extends SubsystemBase {
         return this.runOnce(
                 () -> {
 
-                    boolean isL4ToL3 = (lastSetpoint == Setpoint.L4 && setpoint == Setpoint.L3);
-                    boolean isL3ToL4 = (lastSetpoint == Setpoint.L3 && setpoint == Setpoint.L4);
-                    boolean isL2ToL4 = (lastSetpoint == Setpoint.L2 && setpoint == Setpoint.L4);
-                    boolean isL1ToL4 = (lastSetpoint == Setpoint.L1 && setpoint == Setpoint.L4);
-                    boolean isFeederToL4 = (lastSetpoint == Setpoint.FeederStation && setpoint == Setpoint.L4);
-                    boolean isFeederToL3 = (lastSetpoint == Setpoint.FeederStation && setpoint == Setpoint.L3);
-                    boolean isL4ToAlgaeHigh = (lastSetpoint == Setpoint.L4 && setpoint == Setpoint.AlgaeHigh);
-                    boolean isL4ToAlgaeLow = (lastSetpoint == Setpoint.L4 && setpoint == Setpoint.AlgaeLow);
-                    boolean isL3ToL2 = (lastSetpoint == Setpoint.L3 && setpoint == Setpoint.L2);
-                    boolean isL4ToL2 = (lastSetpoint == Setpoint.L4 && setpoint == Setpoint.L2);
-                    boolean isBargeToL3 = (lastSetpoint == Setpoint.Barge && setpoint == Setpoint.L3);
-                    boolean isBargeToL2 = (lastSetpoint == Setpoint.Barge && setpoint == Setpoint.L2);
-                    boolean isBargeToL1 = (lastSetpoint == Setpoint.Barge && setpoint == Setpoint.L1);
-                    boolean isBargeToFeeder = (lastSetpoint == Setpoint.Barge && setpoint == Setpoint.FeederStation);
-                    if (isL4ToL3 || isL3ToL4 || isL4ToAlgaeHigh || isL4ToAlgaeLow || isL3ToL2 || isL4ToL2 || isFeederToL4 || isL2ToL4 ||isFeederToL3 ||isL1ToL4 || isBargeToL3 || isBargeToL2 || isBargeToL1 || isBargeToFeeder) {
-                        // Apply slow config
-                        r_armMotor.configure(Configs.CoralSubsystem.r_armMotorSlowConfig,
-                                ResetMode.kResetSafeParameters,
-                                PersistMode.kNoPersistParameters);
-                        l_armMotor.configure(Configs.CoralSubsystem.l_armMotorSlowConfig,
-                                ResetMode.kResetSafeParameters,
-                                PersistMode.kNoPersistParameters);
-                    } else {
-                        // Default config
-                        r_armMotor.configure(
-                                Configs.CoralSubsystem.r_armMotorConfig,
-                                ResetMode.kResetSafeParameters,
-                                PersistMode.kNoPersistParameters);
+                    // boolean isL4ToL3 = (lastSetpoint == Setpoint.L4 && setpoint == Setpoint.L3);
+                    // boolean isL3ToL4 = (lastSetpoint == Setpoint.L3 && setpoint == Setpoint.L4);
+                    // boolean isL2ToL4 = (lastSetpoint == Setpoint.L2 && setpoint == Setpoint.L4);
+                    // boolean isL1ToL4 = (lastSetpoint == Setpoint.L1 && setpoint == Setpoint.L4);
+                    // boolean isFeederToL4 = (lastSetpoint == Setpoint.FeederStation && setpoint == Setpoint.L4);
+                    // boolean isFeederToL3 = (lastSetpoint == Setpoint.FeederStation && setpoint == Setpoint.L3);
+                    // boolean isL4ToAlgaeHigh = (lastSetpoint == Setpoint.L4 && setpoint == Setpoint.AlgaeHigh);
+                    // boolean isL4ToAlgaeLow = (lastSetpoint == Setpoint.L4 && setpoint == Setpoint.AlgaeLow);
+                    // boolean isL3ToL2 = (lastSetpoint == Setpoint.L3 && setpoint == Setpoint.L2);
+                    // boolean isL4ToL2 = (lastSetpoint == Setpoint.L4 && setpoint == Setpoint.L2);
+                    // boolean isBargeToL3 = (lastSetpoint == Setpoint.Barge && setpoint == Setpoint.L3);
+                    // boolean isBargeToL2 = (lastSetpoint == Setpoint.Barge && setpoint == Setpoint.L2);
+                    // boolean isBargeToL1 = (lastSetpoint == Setpoint.Barge && setpoint == Setpoint.L1);
+                    // boolean isBargeToFeeder = (lastSetpoint == Setpoint.Barge && setpoint == Setpoint.FeederStation);
+                    // if (isL4ToL3 || isL3ToL4 || isL4ToAlgaeHigh || isL4ToAlgaeLow || isL3ToL2 || isL4ToL2 || isFeederToL4 || isL2ToL4 ||isFeederToL3 ||isL1ToL4 || isBargeToL3 || isBargeToL2 || isBargeToL1 || isBargeToFeeder) {
+                    //     // Apply slow config
+                    //     r_armMotor.configure(Configs.CoralSubsystem.r_armMotorSlowConfig,
+                    //             ResetMode.kResetSafeParameters,
+                    //             PersistMode.kNoPersistParameters);
+                    //     l_armMotor.configure(Configs.CoralSubsystem.l_armMotorSlowConfig,
+                    //             ResetMode.kResetSafeParameters,
+                    //             PersistMode.kNoPersistParameters);
+                    // } else {
+                    //     // Default config
+                    //     r_armMotor.configure(
+                    //             Configs.CoralSubsystem.r_armMotorConfig,
+                    //             ResetMode.kResetSafeParameters,
+                    //             PersistMode.kNoPersistParameters);
 
-                        l_armMotor.configure(
-                                Configs.CoralSubsystem.l_armMotorConfig,
-                                ResetMode.kResetSafeParameters,
-                                PersistMode.kNoPersistParameters);
-                    }
+                    //     l_armMotor.configure(
+                    //             Configs.CoralSubsystem.l_armMotorConfig,
+                    //             ResetMode.kResetSafeParameters,
+                    //             PersistMode.kNoPersistParameters);
+                    // }
 
 
                     switch (setpoint) {
                         case FeederStation:
                             runFunnelIntake = true;
                             ElevatorAtL4 = false;
-                            armCurrentTarget = ArmSetpoints.FeederStation;
+                            //armCurrentTarget = ArmSetpoints.FeederStation;
                             wristCurrentTarget = WristSetpoints.FeederStation;
                             elevatorCurrentTarget = ElevatorSetpoints.FeederStation;
 
@@ -231,7 +231,7 @@ public class CoralSubsystem extends SubsystemBase {
                         case L1:
                             ElevatorAtL4 = false;
                             runFunnelIntake = false;
-                            armCurrentTarget = ArmSetpoints.L1;
+                            //armCurrentTarget = ArmSetpoints.L1;
                             wristCurrentTarget = WristSetpoints.L1;
                             elevatorCurrentTarget = ElevatorSetpoints.L1;
                             break;
@@ -240,40 +240,40 @@ public class CoralSubsystem extends SubsystemBase {
                             ElevatorAtL4 = false;
                             elevatorCurrentTarget = ElevatorSetpoints.AlgaeLow;
                             wristCurrentTarget = WristSetpoints.AlgaeLow;
-                            armCurrentTarget = ArmSetpoints.AlgaeLow;
+                            //armCurrentTarget = ArmSetpoints.AlgaeLow;
                             break;
                         case AlgaeHigh:
                             runFunnelIntake = false;
                             ElevatorAtL4 = false;
                             elevatorCurrentTarget = ElevatorSetpoints.AlgaeHigh;
                             wristCurrentTarget = WristSetpoints.AlgaeHigh;
-                            armCurrentTarget = ArmSetpoints.AlgaeHigh;
+                            //armCurrentTarget = ArmSetpoints.AlgaeHigh;
                             break;
                         case L2:
                             ElevatorAtL4 = false;
                             runFunnelIntake = false;
-                            armCurrentTarget = ArmSetpoints.L2;
+                            //armCurrentTarget = ArmSetpoints.L2;
                             wristCurrentTarget = WristSetpoints.L2;
                             elevatorCurrentTarget = ElevatorSetpoints.L2;
                             break;
                         case L3:
                             ElevatorAtL4 = true;
                             runFunnelIntake = false;
-                            armCurrentTarget = ArmSetpoints.L3;
+                            //armCurrentTarget = ArmSetpoints.L3;
                             wristCurrentTarget = WristSetpoints.L3;
                             elevatorCurrentTarget = ElevatorSetpoints.L3;
                             break;
                         case L4:
                             ElevatorAtL4 = true;
                             runFunnelIntake = false;
-                            armCurrentTarget = ArmSetpoints.L4;
+                            //armCurrentTarget = ArmSetpoints.L4;
                             wristCurrentTarget = WristSetpoints.L4;
                             elevatorCurrentTarget = ElevatorSetpoints.L4;
                             break;
                         case Barge:
                             ElevatorAtL4 = true;
                             runFunnelIntake = false;
-                            armCurrentTarget = ArmSetpoints.Barge;
+                            //armCurrentTarget = ArmSetpoints.Barge;
                             wristCurrentTarget = WristSetpoints.Barge;
                             elevatorCurrentTarget = ElevatorSetpoints.Barge;
                             break;
