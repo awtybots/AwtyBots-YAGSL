c69cfe3948813cb72824c09a8da01d76ab3ad2fb
DavidCui5829 <27cuiz@awty.org>
2025-10-02 21:00:04 -0500

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -335,10 +335,7 @@ public class RobotContainer {
                 new RAlignToReefTagRelative(drivebase),
                 this.scoreUniversal().withTimeout(0.5)
                         .onlyIf(() -> m_rAlignToReefTagRelative.RisAtPose() && m_coralSubsystem.CheckPos())));
-        {
-
-        }
-        ;
+    
         // B Button -> Elevator/Arm to human player position, set ball intake to stow
         // when idle
         operatorFeederStationTrigger.onTrue(m_coralSubsystem.setSetpointCommand(Setpoint.FeederStation, false));
