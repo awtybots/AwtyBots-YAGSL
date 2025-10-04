13d8c3b2d07d5739e088aacbbb4705bab887916a
DavidCui5829 <27cuiz@awty.org>
2025-10-04 16:14:53 -0500

    works for auto

--- src/main/java/frc/robot/commands/RAlignToReefTagRelative.java
+++ src/main/java/frc/robot/commands/RAlignToReefTagRelative.java
@@ -134,7 +134,7 @@ public class RAlignToReefTagRelative extends Command {
             new Translation2d(
                 // If we jump forward before we are centered, increase the Y tolerance gate or
                 // lower this 0.03 safety creep.
-                yController.getError() < 0.3 ? xSpeed : 0.00,
+                yController.getError() < 0.6 ? xSpeed : 0.00,
                 ySpeed),
             rotValue,
             false);
